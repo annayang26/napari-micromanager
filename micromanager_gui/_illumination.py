e346bb2ca744dc2ef4085bfb7feb04d310d1fd28
fdrgsp <federico.gasparoli@gmail.com>
2022-02-22 23:38:01 -0500

    start adding shutter buttons

--- micromanager_gui/_illumination.py
+++ micromanager_gui/_illumination.py
@@ -1,11 +1,37 @@
 import re
+from dataclasses import dataclass
+from typing import Any, Sequence
 
-from magicgui.widgets import Container
+from magicgui.widgets import (
+    ComboBox,
+    Container,
+    FloatSlider,
+    LineEdit,
+    PushButton,
+    Slider,
+    Widget,
+)
+from pymmcore_plus import DeviceType, PropertyType
 from qtpy.QtWidgets import QDialog, QVBoxLayout
 
-from .prop_browser import get_editor_widget, iter_dev_props
+from .prop_browser import iter_dev_props
 
 LIGHT_LIST = re.compile("(Intensity|Power|test)s?", re.IGNORECASE)  # for testing
+
+
+@dataclass
+class PropertyItem:
+    device: str
+    dev_type: DeviceType
+    name: str
+    value: Any
+    read_only: bool
+    pre_init: bool
+    has_range: bool
+    lower_lim: float
+    upper_lim: float
+    prop_type: PropertyType
+    allowed: Sequence[str]
 
 
 class IlluminationDialog(QDialog):
@@ -15,10 +41,49 @@ class IlluminationDialog(QDialog):
         self.setContentsMargins(0, 0, 0, 0)
         self._container = Container(
             widgets=[
-                get_editor_widget(prop, mmcore)
+                self.get_editor_widget(prop, mmcore)
                 for prop in iter_dev_props(mmcore)
                 if LIGHT_LIST.search(prop.name) and prop.has_range
             ],
             labels=True,
         )
         self.layout().addWidget(self._container.native)
+
+    def get_editor_widget(self, prop: PropertyItem, mmc) -> Widget:
+        wdg = None
+        if prop.allowed:
+            wdg = ComboBox(value=prop.value, choices=prop.allowed)
+        elif prop.has_range:
+            if PropertyType(prop.prop_type).name == "Float":
+                wdg = FloatSlider(
+                    value=float(prop.value),
+                    min=float(prop.lower_lim),
+                    max=float(prop.upper_lim),
+                    label=f"{prop.device} {prop.name}",
+                )
+
+                push_button = PushButton(text="TEST")
+
+            else:
+                wdg = Slider(
+                    value=int(prop.value),
+                    min=int(prop.lower_lim),
+                    max=int(prop.upper_lim),
+                    label=f"{prop.device} {prop.name}",
+                )
+
+                push_button = PushButton("TEST")
+        else:
+            wdg = LineEdit(value=prop.value)
+
+        widget = Container(layout="horizontal", widgets=[wdg, push_button], labels=True)
+
+        @wdg.changed.connect
+        def _on_change(value: Any):
+            mmc.setProperty(prop.device, prop.name, value)
+
+        @push_button.clicked.connect
+        def _on_click():
+            print("clicked!")
+
+        return widget
