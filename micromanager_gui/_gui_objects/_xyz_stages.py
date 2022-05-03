03b24e24a8c045577392105f09e1f6f56efb66c6
fdrgsp <federico.gasparoli@gmail.com>
2022-05-03 09:48:09 -0400

    stage order

--- micromanager_gui/_gui_objects/_xyz_stages.py
+++ micromanager_gui/_gui_objects/_xyz_stages.py
@@ -16,20 +16,13 @@ class MMStagesWidget(QWidget):
         super().__init__(parent)
         self.setAcceptDrops(True)
         self.main_layout = QHBoxLayout()
-        self.main_layout.setContentsMargins(10, 10, 10, 10)
-        self.main_layout.setSpacing(6)
+        self.main_layout.setContentsMargins(5, 5, 5, 5)
+        self.main_layout.setSpacing(5)
         self.setLayout(self.main_layout)
 
         self._mmc = _core.get_core_singleton()
         self._on_cfg_loaded()
         self._mmc.events.systemConfigurationLoaded.connect(self._on_cfg_loaded)
-
-        self.setWindowFlags(
-            Qt.WindowType.Window
-            | Qt.WindowType.WindowTitleHint
-            | Qt.WindowType.WindowStaysOnTopHint
-            | Qt.WindowType.WindowCloseButtonHint
-        )
 
     def _on_cfg_loaded(self):
         self._clear()
@@ -100,11 +93,10 @@ class MMStagesWidget(QWidget):
 
 
 class DragGroupBox(QGroupBox):
-    def __init__(self, title: str, start_pos=None) -> None:
+    def __init__(self, name: str, start_pos=None) -> None:
         super().__init__()
-        self._name = title
+        self._name = name
         self.start_pos = start_pos
-        self.setTitle(self._name)
 
     def mouseMoveEvent(self, event):
         drag = QDrag(self)
