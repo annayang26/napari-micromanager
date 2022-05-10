0a0a5e7e3ed88aafc68f242ece7ebaf79a32d478
fdrgsp <federico.gasparoli@gmail.com>
2022-05-10 11:04:41 -0400

    update

--- micromanager_gui/_core_widgets/_snap_button_widget.py
+++ micromanager_gui/_core_widgets/_snap_button_widget.py
@@ -1,7 +1,7 @@
 from typing import Optional, Tuple, Union
 
 from fonticon_mdi6 import MDI6
-from pymmcore_plus import CMMCorePlus, DeviceType
+from pymmcore_plus import CMMCorePlus
 from qtpy.QtCore import QSize, Qt
 from qtpy.QtGui import QColor
 from qtpy.QtWidgets import QPushButton, QSizePolicy
@@ -78,6 +78,10 @@ class SnapButton(QPushButton):
     def _snap(self):
         if self._mmc.isSequenceRunning(self._camera):
             self._mmc.stopSequenceAcquisition(self._camera)
+        if self._mmc.getAutoShutter():
+            self._mmc.events.propertyChanged.emit(
+                self._mmc.getShutterDevice(), "State", True
+            )
         create_worker(self._mmc.snap, _start_thread=True)
 
     def _on_system_cfg_loaded(self):
