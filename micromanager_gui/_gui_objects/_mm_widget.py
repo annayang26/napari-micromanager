5d0d51bbd16ed6c144ec679d3de9c4d69ffb981b
fdrgsp <federico.gasparoli@gmail.com>
2022-05-16 11:00:52 -0400

    update

--- micromanager_gui/_gui_objects/_mm_widget.py
+++ micromanager_gui/_gui_objects/_mm_widget.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from qtpy.QtCore import Qt
 from qtpy.QtWidgets import QGroupBox, QSizePolicy, QVBoxLayout, QWidget
+from superqt import QCollapsible
 
 from .._gui_objects._xyz_stages import MMStagesWidget
 from ._config_widget import MMConfigurationWidget
@@ -35,11 +36,20 @@ class MicroManagerWidget(QWidget):
         self.main_layout.addWidget(self.tab_wdg)
 
     def _create_stage_wdg(self):
-        self.stages = QGroupBox()
-        self.stages.setSizePolicy(QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed))
-        self.stages_layout = QVBoxLayout()
-        self.stages_layout.setSpacing(0)
-        self.stages_layout.setContentsMargins(5, 0, 5, 0)
-        self.stages.setLayout(self.stages_layout)
-        self.stages_layout.addWidget(self.stages_wdg)
-        return self.stages
+
+        stages = QGroupBox()
+        stages.setSizePolicy(QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed))
+        stages_layout = QVBoxLayout()
+        stages_layout.setSpacing(0)
+        stages_layout.setContentsMargins(5, 0, 5, 0)
+        stages.setLayout(stages_layout)
+
+        stages_coll = QCollapsible(title="Stages")
+        stages_coll.layout().setSpacing(0)
+        stages_coll.layout().setContentsMargins(0, 0, 5, 10)
+        stages_coll.addWidget(self.stages_wdg)
+        stages_coll.expand(animate=False)
+
+        stages_layout.addWidget(stages_coll)
+
+        return stages
