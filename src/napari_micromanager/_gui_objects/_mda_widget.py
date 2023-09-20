d9fb5e4d7658127ffadb647a19acdcda72cc68e3
fdrgsp <federico.gasparoli@gmail.com>
2023-09-20 01:47:25 -0400

    HCSWizard

--- src/napari_micromanager/_gui_objects/_mda_widget.py
+++ src/napari_micromanager/_gui_objects/_mda_widget.py
@@ -6,15 +6,18 @@ from typing import TYPE_CHECKING, cast
 
 from pymmcore_mda_writers import MultiTiffWriter
 from pymmcore_widgets import MDAWidget
+from pymmcore_widgets._hcs_widget._main_wizard_widget import HCSWizard
+from qtpy.QtCore import Signal
 from qtpy.QtWidgets import (
     QCheckBox,
     QGridLayout,
     QMessageBox,
+    QPushButton,
     QSizePolicy,
     QVBoxLayout,
     QWidget,
 )
-from useq import MDASequence
+from useq import AxesBasedAF, MDASequence
 
 from napari_micromanager._mda_meta import SEQUENCE_META_KEY, SequenceMeta
 
@@ -22,6 +25,57 @@ from ._save_widget import SaveWidget
 
 if TYPE_CHECKING:
     from pymmcore_plus import CMMCorePlus
+    from pymmcore_widgets import PositionTable
+
+
+class HCSWidget(HCSWizard):
+    valueChanged = Signal(object)
+
+    def __init__(
+        self,
+        parent: QWidget | None = None,
+        *,
+        mmcore: CMMCorePlus | None = None,
+        plate_database_path: Path | str | None = None,
+        position_table: PositionTable,
+    ) -> None:
+        super().__init__(parent, mmcore=mmcore, plate_database_path=plate_database_path)
+
+        self._pt = position_table
+
+    def accept(self) -> None:
+        """Override QWizard default accept method."""
+        print(self.value())
+
+        well_centers = self._get_well_center_in_stage_coordinates()
+        if well_centers is None:
+            return
+        _positions = self._get_fovs_in_stage_coords(well_centers)
+
+        # update with current z position and z autofocus if `Locked`
+        positions = []
+        for pos in _positions:
+            if self._mmc.getFocusDevice():
+                pos = pos.replace(z=self._mmc.getPosition(self._mmc.getFocusDevice()))
+
+            if self._mmc.isContinuousFocusLocked():
+                af_z_device = self._pt._get_af_device()
+                if af_z_device is not None:
+                    pos = pos.replace(
+                        sequence=MDASequence(
+                            autofocus_plan=AxesBasedAF(
+                                axes=("t", "p", "g"),
+                                autofocus_device_name=af_z_device,
+                                autofocus_motor_offset=self._mmc.getPosition(
+                                    af_z_device
+                                ),
+                            )
+                        )
+                    )
+
+            positions.append(pos)
+
+        self.valueChanged.emit(positions)
 
 
 class MultiDWidget(MDAWidget):
@@ -33,6 +87,13 @@ class MultiDWidget(MDAWidget):
         super().__init__(include_run_button=True, parent=parent, mmcore=mmcore)
 
         self._tiff_writer = MultiTiffWriter(core=mmcore)
+
+        # add HCS button
+        self._hcs_btn = QPushButton(text="HCS")
+        self._hcs_btn.clicked.connect(self._on_hcs_clicked)
+        self.position_widget.layout().itemAt(1).widget().layout().insertWidget(
+            7, self._hcs_btn
+        )
 
         # add save widget
         v_layout = cast(QVBoxLayout, self._central_widget.layout())
@@ -53,6 +114,13 @@ class MultiDWidget(MDAWidget):
         g_layout = cast(QGridLayout, self.channel_widget.layout())
         g_layout.addWidget(self.checkBox_split_channels, 1, 0)
         self.channel_widget.valueChanged.connect(self._toggle_split_channel)
+
+    def _on_hcs_clicked(self) -> None:
+        if not hasattr(self, "_hcs"):
+            self._hcs = HCSWidget(position_table=self.position_widget)
+            self._hcs.valueChanged.connect(lambda x: self.position_widget.set_state(x))
+        self._hcs.show()
+        self._hcs.raise_()
 
     def _toggle_split_channel(self) -> None:
         if (
