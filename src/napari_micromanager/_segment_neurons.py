4ddc1a969b2bdb36c479988f6ebef728374d5f35
Afshar Saber <wardiya.afsharsaber@childrens.harvard.edu>
2024-03-05 10:33:57 -0500

    fix: spelling

--- src/napari_micromanager/_segment_neurons.py
+++ src/napari_micromanager/_segment_neurons.py
@@ -2,7 +2,7 @@ import numpy as np
 import useq
 from pymmcore_plus import CMMCorePlus
 from superqt.utils import create_worker
-
+from pathlib import Path
 
 class SegmentNeurons:
     """Segment neurons."""
@@ -14,15 +14,22 @@ class SegmentNeurons:
         self._mmc.mda.events.frameReady.connect(self._on_frame_ready)
         self._mmc.mda.events.sequenceFinished.connect(self._on_sequence_finished)
 
+        self._path = Path(r"C:\Users\CH167448\Desktop\test")
+
+        self._fname: str = ""
+
     def _on_sequence_started(self, sequence: useq.MDASequence) -> None:
         print("\nSEQUENCE STARTED")
+        meta = sequence.metadata.get("pymmcore_wigets")
+        if meta is not None:
+            self._fname = meta.get("save_name", "")
 
     def _on_frame_ready(self, image: np.ndarray, event: useq.MDAEvent) -> None:
         print("FRAME READY", event.index)
         t_index = event.index.get("t")
         # perform segmentation every first timepoint
         if t_index is not None and t_index == 0:
-            print("     PREFORM IMAGE SEGMENTATION")
+            print("     PERFORM IMAGE SEGMENTATION")
             create_worker(
                 self._segment_image,
                 image,
@@ -36,6 +43,9 @@ class SegmentNeurons:
     def _segment_image(self, image: np.ndarray) -> None:
         """Segment the image."""
         print("     SEGMENTING IMAGE", image.shape)
+        print(self._mmc.getPixelSizeUm())
+        if self._fname:
+            ...
 
     def _segmentation_finished(self) -> None:
         print("     SEGMENTATION FINISHED")
