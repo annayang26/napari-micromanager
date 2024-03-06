1da136613016cbdece42fbfa567ac8f72617ed65
Anna Yang <annayangzq26@gmail.com>
2024-03-06 16:02:34 -0500

    able to load model and save the pred img; changed normalization; include clear; need to test segmentation

--- src/napari_micromanager/_segment_neurons.py
+++ src/napari_micromanager/_segment_neurons.py
@@ -19,16 +19,21 @@ class SegmentNeurons:
         self._mmc.mda.events.sequenceFinished.connect(self._on_sequence_finished)
 
         self._path: str = ""
-        self._fname: str = ""
+        self._exp_name: str = ""
         self._model_size = 0
         self._model_unet = None
 
+        self._current_pos: int = 0
+        self._total_pos: int = 0
+
     def _on_sequence_started(self, sequence: useq.MDASequence) -> None:
         print("\nSEQUENCE STARTED")
-        meta = sequence.metadata.get("pymmcore_wigets")
+        meta = sequence.metadata.get("pymmcore_widgets")
+        num_pos = len(sequence.stage_positions)
         if meta is not None:
-            self._fname = meta.get("save_name", "")
-            self._path = meta.get("save_dir", "") ### TODO: Need to double check
+            self._exp_name = meta.get("save_name", "")
+            self._path = meta.get("save_dir", "")
+            self._total_pos = num_pos
 
     def _on_frame_ready(self, image: np.ndarray, event: useq.MDAEvent) -> None:
         print("FRAME READY", event.index)
@@ -43,12 +48,10 @@ class SegmentNeurons:
                 import tensorflow.keras.backend as K
 
                 img_size = image.shape[0]
+                self._model_size = img_size
 
-                ### TODO: try os.path for now; change to Pathlib later
                 dir_path = Path(__file__).parent
                 path = Path.joinpath(dir_path, f'unet_calcium_{img_size}')
-                # dir_path = os.path.dirname(os.path.realpath(__file__))
-                # path = os.path.join(dir_path, f'unet_calcium_{img_size}.hdf5')
                 self._model_unet = tf.keras.models.load_model(path,
                                                               custom_objects={"K": K})
 
@@ -56,6 +59,7 @@ class SegmentNeurons:
                 self._segment_image,
                 image,
                 self._model_unet,
+                self._exp_name,
                 _start_thread=True,
                 _connect={"finished": self._segmentation_finished},
             )
@@ -63,14 +67,14 @@ class SegmentNeurons:
     def _on_sequence_finished(self, sequence: useq.MDASequence) -> None:
         print("\nSEQUENCE FINISHED")
 
-    def _segment_image(self, image: np.ndarray, model) -> None:
+    def _segment_image(self, image: np.ndarray, model, exp_name: str) -> None:
         """Segment the image."""
         print("     SEGMENTING IMAGE", image.shape)
         # print(self._mmc.getPixelSizeUm())
-        print(f"FILENAME: {self._fname}, the model initiated: {self._model_unet}")
         if model is not None:
-            img_norm = np.max(image, axis=0) / np.max(image)
-            img_predict = self._model_unet.predict(img_norm[np.newaxis, :])[0, :, :] #somthing about this is not working
+            # img_norm = np.max(image, axis=0) / np.max(image)
+            img_norm = self._normalize_img(image)
+            img_predict = model.predict(img_norm[None, :, :])[:, :]
 
             # if np.max(img_predict) > 0.3:
             #     # the prediction layer shows the prediction of the NN
@@ -102,17 +106,51 @@ class SegmentNeurons:
             #     labels, label_layer, roi_dict = None, None, None
 
         # return labels, label_layer, roi_dict
-        print("prediction shape: ", img_predict.shape)
         # save the prediction
-        self._save_img(img_predict)
+        self._save_img(img_predict, exp_name)
         print("     Prediction Image saved!")
 
-    def _save_img(self, pred_img: np.ndarray):
+    def _normalize_img(self, img):
+        '''
+        '''
+        g_max = np.max(img)
+        g_min = np.min(img)
+        img_norm = (img - g_min)/(g_max - g_min)
+        return img_norm
+
+    def _save_img(self, pred_img: np.ndarray, exp_name):
+        pred_img = pred_img.reshape((pred_img.shape[1], pred_img.shape[2]))
         im_pred = Image.fromarray(np.uint8(pred_img*255), mode='L')
-        if len(self._fname) == 0:
-            self._fname = "test"
-        pred_path = Path.joinpath(self._path, self._fname, '_Prediction.png')
+        exp_name += ("_Pos" + str(self._current_pos) + "_PREDICTION.png")
+        pred_path = Path(self._path).joinpath(exp_name)
         im_pred.save(pred_path)
+    
+    def _change_position(self):
+        self._current_pos += 1
+
+    def _clear(self):
+        self._path: str = ""
+        self._exp_name: str = ""
+        self._model_size = 0
+        self._model_unet = None
+
+        self._current_pos = 0
+        self._total_pos = 0
 
     def _segmentation_finished(self) -> None:
         print("     SEGMENTATION FINISHED")
+        self._change_position()
+        if self._current_pos == self._total_pos:
+            self._clear()
+
+'''
+TODO list (03/06):
+    1. figure out how to get the save path and the file name --- done
+    2. make sure that the segmentation is working well. 
+        changes:
+        - the way to normalize the image (from divided by a global 
+            max to min-max normalization)
+        - the shape of the prediction image
+    3. the output wanted after segmentation. just image? roi_dict? 
+
+'''
