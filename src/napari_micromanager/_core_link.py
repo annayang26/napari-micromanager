b4b54ba0655fc17070d727ae3eebdad4e807c262
Anna Yang <annayangzq26@gmail.com>
2024-04-24 09:47:23 -0400

    fx: wip

--- src/napari_micromanager/_core_link.py
+++ src/napari_micromanager/_core_link.py
@@ -33,7 +33,7 @@ class CoreViewerLink(QObject):
         self._mda_handler = _NapariMDAHandler(self._mmc, viewer)
         self._live_timer_id: int | None = None
 
-        self._segment_neurons = SegmentNeurons(self._mmc)
+        # self._segment_neurons = SegmentNeurons(self._mmc)
 
         # Add all core connections to this list.  This makes it easy to disconnect
         # from core when this widget is closed.
